a97ce79d6503da2682b54308bb6615448d68cdcb
Micaela Quiroga <micaelaquiroga027@gmail.com>
2024-12-10 14:01:12 -0300

    contador-letras.py

---
+++ contador.letras.py
@@ -0,0 +1,9 @@
+# Paso 1: solicitar entrada al usuario
+
+palabra_ingresada = input("Hola, por favor ingrese una palabra: ")
+
+# Paso 2: contar la cantidad de letras
+cantidad_letras = len(palabra_ingresada)
+
+# Paso 3: Mostrar al usuario el resultado
+print("La palabra ingresada tiene ", cantidad_letras, "letras")
